e9a81d80bd00eb2eb1d9e6be40bb3c3f492a13b8
jakemaxell <37555603+jakemaxell@users.noreply.github.com>
2022-08-10 14:16:18 -0400

    Add files via upload

--- CollegeStudent/src/main/java/com/example/CollegeStudent/CollegeStudentApplication.java
+++ CollegeStudent/src/main/java/com/example/CollegeStudent/CollegeStudentApplication.java
@@ -2,8 +2,10 @@ package com.example.CollegeStudent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CollegeStudentApplication {
 
 	public static void main(String[] args) {
